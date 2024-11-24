fe25ec40aeb999e9513f3d6aac88d46524910605
minhtaile2712 <minhtaile2712@gmail.com>
2024-11-24 17:58:33 +0700

    Create modal

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,11 +21,7 @@ export default function TabLayout() {
         options={{
           title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
+            <Ionicons name={focused ? "home-sharp" : "home-outline"} color={color} size={24} />
           ),
         }}
       />
@@ -34,13 +30,7 @@ export default function TabLayout() {
         options={{
           title: "About",
           tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={
-                focused ? "information-circle" : "information-circle-outline"
-              }
-              color={color}
-              size={24}
-            />
+            <Ionicons name={focused ? "information-circle" : "information-circle-outline"} color={color} size={24} />
           ),
         }}
       />
